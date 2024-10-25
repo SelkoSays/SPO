e37974f322dcf3cadf16229d6b41632f0381096d
SelkoSays <70065171+SelkoSays@users.noreply.github.com>
2024-10-25 16:21:00 +0200

    WORKING FOR NOW

--- bouncing-balls/bb.asm
+++ bouncing-balls/bb.asm
@@ -2,9 +2,185 @@ BB	START	0
 PROG
 	JSUB	stkinit
 	JSUB	ballInit
-	
 
+lLoop
+. Clear balls
+	. CLEAR	A
+	. STA	arg1
+	. JSUB	dspBalls
+
+. Move balls
+	JSUB	mvBalls
+
+. Display balls
+	. LDA	#111	. 111 = 'o'
+	. STA	arg1
+	. JSUB	dspBalls
+	
+	J	lLoop
+	
 halt	J	halt
+
+. Display balls on screen
+. char = arg1
+dspBalls
+	STL	@stkptr
+	JSUB	PUSH
+	STA	@stkptr
+	JSUB	PUSH
+	STX	@stkptr
+	JSUB	PUSH
+	STT	@stkptr
+	JSUB	PUSH
+
+	LDX	#balls
+	LDA	#bLen
+	MUL	#bSize
+	ADD	#balls
+	RMO	A, T
+
+dbLoop	COMPR	X, T
+	JEQ	dbEndLoop
+	JGT	dbEndLoop
+
+	. X POS
+	LDA	0, X
+	STA	arg2
+	
+	. Y POS
+	LDA	3, X
+	STA	arg3
+
+	JSUB	dspChar
+
+	LDA	#bSize	. SKIP BALL
+	ADDR	A, X
+
+	J	dbLoop	
+
+dbEndLoop
+	JSUB	POP
+	LDT	@stkptr
+	JSUB	POP
+	LDX	@stkptr
+	JSUB	POP
+	LDA	@stkptr
+	JSUB	POP
+	LDL	@stkptr
+
+	RSUB
+
+. Move balls
+mvBalls
+	STL	@stkptr
+	JSUB	PUSH
+	STA	@stkptr
+	JSUB	PUSH
+	STX	@stkptr
+	JSUB	PUSH
+	STT	@stkptr
+	JSUB	PUSH
+
+	LDX	#balls
+	LDA	#bLen
+	MUL	#bSize
+	ADD	#balls
+	RMO	A, T
+
+mvbLoop
+	COMPR	X, T
+	JEQ	mvbEndLoop
+	JGT	mvbEndLoop
+
+. ERASE BALL'S LAST POS ON SCREEN
+.--------------------------------------
+	LDA	#0
+	STA	arg1
+	
+	. X POS
+	LDA	0, X
+	STA	arg2
+	
+	. Y POS
+	LDA	3, X
+	STA	arg3
+
+	JSUB	dspChar
+.--------------------------------------
+
+. CHANGE X
+.--------------------------------------
+mvbXP	LDA	0, X	. LOAD X POS
+	ADD	6, X	. ADD  X VEL
+
+	. SHOULD BE 0 <= Y < HEIGHT
+	COMP	#0
+	JLT	mvbCXVS
+	COMP	#width
+	JLT	mvbSkipX
+
+mvbCXVS . change X velocity sign
+	CLEAR 	A
+	SUB	#1
+	MUL	6, X
+	STA	6, X
+	J	mvbYP
+mvbSkipX
+	STA	0, X	. STORE NEW X POS
+.--------------------------------------
+
+. CHANGE Y
+.--------------------------------------
+mvbYP	LDA	3, X	. LOAD Y POS
+	ADD	9, X	. ADD  Y VEL
+
+	. SHOULD BE 0 <= Y < HEIGHT
+	COMP	#0
+	JLT	mvbCYVS
+	COMP	#height
+	JLT	mvbSkipY
+
+mvbCYVS . change Y velocity sign
+	CLEAR 	A
+	SUB	#1
+	MUL	9, X
+	STA	9, X
+	J	mvbYP
+
+mvbSkipY
+	STA	3, X	. STORE NEW Y POS
+.--------------------------------------
+
+. DISPLAY BALL ON SCREEN
+.--------------------------------------
+	LDA	#111	. 111 = 'o'
+	STA	arg1
+	
+	. X POS
+	LDA	0, X
+	STA	arg2
+	
+	. Y POS
+	LDA	3, X
+	STA	arg3
+
+	JSUB	dspChar
+.--------------------------------------
+
+	LDA	#bSize
+	ADDR	A, X
+	J	mvbLoop
+
+mvbEndLoop
+	JSUB	POP
+	LDT	@stkptr
+	JSUB	POP
+	LDX	@stkptr
+	JSUB	POP
+	LDA	@stkptr
+	JSUB	POP
+	LDL	@stkptr
+	RSUB
 
 . Init Balls
 ballInit
@@ -13,8 +189,6 @@ ballInit
 	STA	@stkptr
 	JSUB	PUSH
 	STX	@stkptr
-	JSUB	PUSH
-	STB	@stkptr
 	JSUB	PUSH
 	STT	@stkptr
 	JSUB	PUSH
@@ -54,28 +228,38 @@ bLoop	COMPR	X, T
 	ADDR	A, X
 
 	. RANDOM X VEL
-	+LDA	#0xFFFFF
-	SHIFTL	A, 4
-	OR	#0xF
+	CLEAR	A
 	STA	arg1
 	LDA	#2
 	STA	arg2
 	JSUB	rand
 	LDA	arg1
+	MUL	#3
+	. choose -1 or 1
+	STX	svx
+	LDX	#CHOICE
+	ADDR	A, X
+	LDA	0, X
+	LDX	svx
 	STA	0, X
 
 	LDA	#3
 	ADDR	A, X
 
 	. RANDOM Y VEL
-	+LDA	#0xFFFFF
-	SHIFTL	A, 4
-	OR	#0xF
+	CLEAR	A
 	STA	arg1
 	LDA	#2
 	STA	arg2
 	JSUB	rand
 	LDA	arg1
+	MUL	#3
+	. choose -1 or 1
+	STX	svx
+	LDX	#CHOICE
+	ADDR	A, X
+	LDA	0, X
+	LDX	svx
 	STA	0, X
 
 	LDA	#3
@@ -86,8 +270,6 @@ bEndLoop
 
 	JSUB	POP
 	LDT	@stkptr
-	JSUB	POP
-	LDB	@stkptr
 	JSUB	POP
 	LDX	@stkptr
 	JSUB	POP
@@ -225,15 +407,45 @@ blk_u_r	TD	#1
 	JGT	blk_u_r
 	RSUB
 
+. Display char on screen
+. arg1 = char, arg2 = xpos, arg3 = ypos
+dspChar
+	STL	@stkptr
+	JSUB	PUSH
+	STA	@stkptr
+	JSUB	PUSH
+	STX	@stkptr
+	JSUB	PUSH
+
+	. IDX = (WIDTH * Y) + X
+	LDA	#width
+	MUL	arg3
+	ADD	arg2
+	+ADD	#scraddr
+
+	RMO	A, X
+	LDA	arg1
+	STCH	0, X
+
+	JSUB	POP
+	LDX	@stkptr
+	JSUB	POP
+	LDA	@stkptr
+	JSUB	POP
+	LDL	@stkptr
+
+	RSUB
+
 .DATA
 
 .screen
 width	EQU	80
 height	EQU	25
 pxlen	EQU	width * height
+scraddr	EQU	0xB800
 
 . rand data
-rnd_cur	WORD	142	. SEED
+rnd_cur	WORD	143	. SEED
 rnd_mul	WORD	53
 rnd_add	WORD	13
 
@@ -246,9 +458,13 @@ b_py	WORD	3	. offset of field py
 b_vx	WORD	6	. offset of field vx
 b_vy	WORD	9	. offset of field vy
 
-bLen	EQU	3
+bLen	EQU	1
 bSize	EQU	12
 balls	RESW	bLen * bSize . array of balls
+
+svx	RESW	1
+CHOICE	WORD	-1
+	WORD	1
 
 .stack
 stksv	RESW	1
@@ -268,8 +484,5 @@ stkuf_s	BYTE	C'Stack Underflow'
 . Args
 arg1	WORD	0
 arg2	WORD	0
-
-. Retturn Values
-ret1	WORD	0
-
+arg3	WORD	0
 	END	PROG
